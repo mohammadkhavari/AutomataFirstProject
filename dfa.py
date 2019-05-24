6effc75a1435bc01f21270edb892e8509e017076
mohammadkhavari <mse737.khavari@gmail.com>
2019-05-24 14:01:57 +0430

    complete minimzation dfa

--- dfa.py
+++ dfa.py
@@ -3,11 +3,12 @@ from nfa import NFA
 class DFA(NFA):
 
     def minimize(self):
-        self.remove_unreachable_states()
+        # find all reachable states and do the minimization peocess just for reachables
+        reachables = self.get_reachable_states()
         # prevent to ignore trapa cuase they'll become one state
         classes = [{},{}]
         state_to_class = {}
-        for state in range(0, self.numofStates):
+        for state in reachables:
             if state in self.finalState:
                 classes[1][state] = self.transition_per_alphabet(state)
                 state_to_class[state] = 1
@@ -39,13 +40,14 @@ class DFA(NFA):
                 classes = []
                 for i in range(0, class_count):
                     class_ = {}
-                    for state in range(0, self.numofStates):
+                    for state in reachables:
                         if state_to_class[state] == i:
                             class_[state] = self.transition_per_alphabet(state)
                     classes.append(class_)
-            self.construct_minimize(classes, state_to_class)
+            self.minimization(classes, state_to_class)
 
-    def construct_minimize(self, classes, state_to_class):
+
+    def minimization(self, classes, state_to_class):
         finals = self.finalState
         start = self.instance
         self.transitions = {}
@@ -60,7 +62,7 @@ class DFA(NFA):
             for i in range(0, len(self.alphabet)):
                 self.add(state_to_class[state], self.alphabet[i], state_to_class[nextStates[i]])
 
-    def remove_unreachable_states(self):
+    def get_reachable_states(self):
         from queue import Queue
         reachables = set()
         q = Queue()
@@ -68,47 +70,16 @@ class DFA(NFA):
         while q.qsize() > 0:
             state = q.get()
             reachables.add(state)
-            for label, nextState in self.transitions[state]:
+            for _, nextState in self.transitions[state]:
                 if nextState not in reachables:
                     q.put(nextState)
-        # now we reconstruct DFA with reachable states
-        self.reconstruct(reachables)
+        return reachables
 
     def still_grouped(self,state_transitions1, state_transitions2, state_to_class):
         for i in range(0, len(state_transitions1)):
             if state_to_class[state_transitions1[i]] != state_to_class[state_transitions2[i]]:
                 return False
         return True
-
-    def reconstruct(self, reachables):
-        oldDFA = self
-        dfaContent = str(oldDFA)
-        # reset self and reconstruct
-        self.transitions = {}
-        self.instance = None
-        self.finalState = []
-        self.numofStates = None
-        self.alphabet = []
-        self.transitions = {}
-
-        lines = dfaContent.split('\n')
-        self.numofStates = len(reachables)
-        self.alphabet = lines[1].split(',')
-        lines = lines[2:]
-        for i in range(0, self.numofStates):
-            self.transitions[i] = set()
-        for line in lines:
-            parts = line.split(',')
-            statesInt = []
-            for state in [parts[0], parts[2]] :
-                tmp = int(state.split('q')[1])
-                statesInt.append(tmp)
-                if '->' in state:
-                    self.instance = tmp
-                if '*' in state and tmp not in self.finalState:
-                    self.finalState.append(tmp)
-            if statesInt[0] in reachables and statesInt[1] in reachables:
-                self.add(statesInt[0], parts[1], statesInt[1])
 
     def transition_per_alphabet(self, state):
         label_to_state = {}
